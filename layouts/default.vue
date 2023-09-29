854dd8b6c237255a2415887d6f1aeb2dc9330fcf
Shion Ichikawa <shion1305@gmail.com>
2023-09-30 07:47:38 +0900

    ✨ 背景の設定

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <slot/>
+  </div>
+</template>
+
+<style>
+body {
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(45deg,  #05273c, #4f1344),
+        linear-gradient(rgba(17, 22, 105, 0.07), rgba(8, 9, 70, 0.5));
+  background-blend-mode: screen;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      link:[
+        {
+          rel: "stylesheet",
+          href: "https://unpkg.com/normalize.css"
+        }
+      ]
+    }
+  }
 })
