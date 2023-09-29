9bc7fab10c42ed6f958e76f8433bc000f4d72a4a
Shion Ichikawa <shion1305@gmail.com>
2023-09-30 08:05:06 +0900

    💄 (layout/default)サイズ、文字色の調整

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,10 +8,11 @@
 
 <style>
 body {
-  width: 100vw;
-  height: 100vh;
+  width: 100lvw;
+  height: 100lvh;
   background: linear-gradient(45deg,  #05273c, #4f1344),
         linear-gradient(rgba(17, 22, 105, 0.07), rgba(8, 9, 70, 0.5));
   background-blend-mode: screen;
+  color: white;
 }
 </style>
