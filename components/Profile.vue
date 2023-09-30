22f7883b278eb8e0c9a6c5bf589be4912f662299
Shion Ichikawa <shion1305@gmail.com>
2023-09-30 09:01:45 +0900

    ✨ (component)Profile

---
+++ components/Profile.vue
@@ -0,0 +1,108 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="profile">
+    <div class="name">
+      <h2 class="jp">市川 詩恩</h2>
+      <p class="en">Shion Ichikawa</p>
+    </div>
+    <div class="status">
+      <div class="main">
+        <p>横浜国立大学(YNU)</p>
+        <p>理工学部数物電子情報系学科</p>
+        <p>情報工学EP 学部3年</p>
+      </div>
+      <hr />
+      <ul>
+        <li>横浜国立大学&nbsp;吉岡研究室</li>
+        <li>
+          <span>Settlementチーム</span>
+          <span>@メルカリ・メルペイ</span>
+        </li>
+        <li>
+          <span>金融チーム</span>
+          <span>@東京大学松尾研究室</span>
+        </li>
+        <li>開発インターン@Poetics</li>
+        <li>YNU大学祭実行委員会技術部局長</li>
+        <li>Lumos&nbsp;<sub>(プログラミングサークル)</sub></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.name {
+  text-align: center;
+
+  * {
+    margin: 0;
+  }
+
+  .jp {
+    font-family: "Klee One", sans-serif;
+    font-size: 2em;
+  }
+
+  .en {
+    font-size: 1.3em;
+  }
+}
+
+.status {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
+  margin: 1em 0;
+  border: 1px solid white;
+  border-radius: 1em;
+  padding: 0.75em;
+
+  .main {
+    font-size: 1.2em;
+    text-align: center;
+
+    > p {
+      margin: 0;
+    }
+
+    > span,
+    > sub {
+      display: inline-block;
+    }
+  }
+
+  > hr {
+    margin: 0.5em 0;
+  }
+
+  > ul {
+    font-size: 0.8em;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 0.65em;
+    text-align: center;
+
+    > li {
+      > span {
+        display: inline-block;
+        width: fit-content;
+        border-radius: 0.5em;
+      }
+    }
+  }
+}
+
+.profile {
+  width: fit-content;
+  padding: 1em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: min(1.3em, 5vw);
+  align-self: center;
+}
+</style>
