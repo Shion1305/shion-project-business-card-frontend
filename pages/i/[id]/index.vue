31ec0283403efa7237a4bf1a2720176794b7d7da
Shion Ichikawa <shion1305@gmail.com>
2023-09-30 10:24:13 +0900

    ✨ (page)index,i/[id]/index

---
+++ pages/i/[id]/index.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="page-root">
+    <Profile />
+    <div class="sns-area">
+      <SNSItem snsType="github" />
+      <SNSItem snsType="gmail" />
+      <SNSItem snsType="x" />
+      <SNSItem snsType="discord" />
+      <SNSItem snsType="line" />
+      <SNSItem snsType="wantedly" />
+      <SNSItem snsType="linkedin" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.page-root {
+  display: flex;
+  flex-direction: column;
+}
+
+.sns-area {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.sns-item {
+  width: 300px;
+  height: 50px;
+}
+</style>
