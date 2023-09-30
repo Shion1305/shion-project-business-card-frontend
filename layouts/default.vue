a2bcf49cfb699e932c9fb472377a9941a11b67e7
Shion Ichikawa <shion1305@gmail.com>
2023-09-30 23:18:20 +0900

    🐛 (layout/default) スクロールの対応

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    <slot/>
+  <div class="layout-root">
+    <slot />
   </div>
 </template>
 
@@ -10,10 +10,16 @@
 body {
   width: 100lvw;
   height: 100lvh;
-  background: linear-gradient(45deg,  #05273c, #4f1344),
-        linear-gradient(rgba(17, 22, 105, 0.07), rgba(8, 9, 70, 0.5));
+  background: linear-gradient(45deg, #05273c, #4f1344),
+    linear-gradient(rgba(17, 22, 105, 0.07), rgba(8, 9, 70, 0.5));
   background-blend-mode: screen;
   color: white;
-  font-family: 'Noto Sans JP', sans-serif;
+  font-family: "Noto Sans JP", sans-serif;
+}
+
+.layout-root {
+  width: 100lvw;
+  height: 100lvh;
+  overflow: scroll;
 }
 </style>
