e192b213994dd0effd10123bc5f9810d77cfd361
Shion Ichikawa <shion1305@gmail.com>
2023-09-30 22:56:18 +0900

    ✨ (server/pkg)firebase-admin

---
+++ server/pkg/firebase-admin.ts
@@ -0,0 +1,11 @@
+import admin from "firebase-admin";
+import * as fs from "fs";
+
+const config = useRuntimeConfig();
+// read json file from FileSystem and parse it as json object
+const credential = JSON.parse(fs.readFileSync(config.gcp.certPath, "utf8"));
+admin.initializeApp({
+  credential: admin.credential.cert(credential as admin.ServiceAccount),
+});
+
+export default admin;
